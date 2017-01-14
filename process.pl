a98503a8504a8595f034a009b20a578bbc04b9d4
Dan Martin <dan@dan-martin.net>
2017-01-14 14:24:33 +0000

    simple script reads and writes files

--- process.pl
+++ process.pl
@@ -2,11 +2,13 @@
 
 use strict;
 
-my ($infile,$outfile) = $_;
+# command line args
+my ($infile,$outfile) = @ARGV;
 
+print "Input file: $infile\n Output file: $outfile\n";
 
 # read the file
-open my $INH, $infile or die "Unable to read input file\n";
+open my $INH, "<", $infile or die "Unable to read input file\n";
 
 chomp(my @lines = <$INH>);
 
@@ -14,7 +16,7 @@ close $INH;
 
 # write the file
 open my $OUTH, ">", $outfile or die "Unable to write output file";
-print $OUTH, join("\n", @lines);
+print $OUTH join("\n", @lines);
 
 # done
 print "Done!\n";
